83eda9bda66623c07987de926aaf834e9b344659
Mytch <m.mason@gosolo.io>
2022-03-02 15:43:54 -0700

    add ability to change fftSize and smoothingTimeConstant

--- src/AudioSpectrum.tsx
+++ src/AudioSpectrum.tsx
@@ -17,6 +17,8 @@ export type AudioSpectrumProps = {
   meterCount: number;
   meterColor: string | MeterColor[];
   gap: number;
+  smoothingTimeConstant?: number;
+  fftSize?: number;
 } & React.HTMLProps<HTMLCanvasElement>;
 
 type PlayStatus = "PAUSED" | "PLAYING";
@@ -34,6 +36,8 @@ const defaultProps = {
     { stop: 1, color: "red" },
   ],
   gap: 10,
+  smoothingTimeConstant: 0.8,
+  fftSize: 2048,
 };
 
 function getRandomId(len: number) {
@@ -53,6 +57,8 @@ export default function AudioSpectrum({
   meterWidth = defaultProps.meterWidth,
   meterCount = defaultProps.meterCount,
   meterColor = defaultProps.meterColor,
+  fftSize = defaultProps.fftSize,
+  smoothingTimeConstant = defaultProps.smoothingTimeConstant,
   gap = defaultProps.gap,
   id = getRandomId(50),
   audioEle: propsAudioEl,
@@ -80,98 +86,121 @@ export default function AudioSpectrum({
       audioEle.current = propsAudioEl;
     }
     audioCanvas.current = canvasRef.current;
-  }, [audioId, propsAudioEl]);
+  }, [audioId, propsAudioEl, meterColor]);
 
-  const drawSpectrum = React.useCallback(
-    (currentAnalyser: AnalyserNode) => {
-      const cWidth = audioCanvas.current!.width;
-      const cHeight = audioCanvas.current!.height - capHeight;
-      const capYPositionArray: number[] = [];
-      const ctx = audioCanvas.current!.getContext("2d")!;
-      const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+  const drawSpectrum = (currentAnalyser: AnalyserNode) => {
+    const cWidth = audioCanvas.current!.width;
+    const cHeight = audioCanvas.current!.height - capHeight;
+    const capYPositionArray: number[] = [];
+    const ctx = audioCanvas.current!.getContext("2d")!;
+    const gradient = ctx.createLinearGradient(0, 0, 0, 300);
 
-      if (Array.isArray(meterColor)) {
-        const stops = meterColor;
-        stops.forEach((stop) => {
-          gradient.addColorStop(stop.stop, stop.color as string);
-        });
-      } else if (typeof meterColor === "string") {
-        gradient.addColorStop(0, meterColor);
+    if (Array.isArray(meterColor)) {
+      const stops = meterColor;
+      stops.forEach((stop) => {
+        gradient.addColorStop(stop.stop, stop.color as string);
+      });
+    } else if (typeof meterColor === "string") {
+      gradient.addColorStop(0, meterColor);
+    }
+
+    const drawMeter = () => {
+      const array = new Uint8Array(currentAnalyser.frequencyBinCount);
+      currentAnalyser.getByteFrequencyData(array);
+      if (playStatus.current === "PAUSED") {
+        array.fill(0);
+        const allCapsReachBottom = !capYPositionArray.some((cap) => cap > 0);
+        if (allCapsReachBottom) {
+          ctx.clearRect(0, 0, cWidth, cHeight + capHeight);
+          cancelAnimationFrame(animationId.current!);
+          return;
+        }
       }
-
-      const drawMeter = () => {
-        const array = new Uint8Array(currentAnalyser.frequencyBinCount);
-        currentAnalyser.getByteFrequencyData(array);
-        if (playStatus.current === "PAUSED") {
-          array.fill(0);
-          const allCapsReachBottom = !capYPositionArray.some((cap) => cap > 0);
-          if (allCapsReachBottom) {
-            ctx.clearRect(0, 0, cWidth, cHeight + capHeight);
-            cancelAnimationFrame(animationId.current!);
-            return;
-          }
+      const step = Math.round(array.length / meterCount);
+      ctx.clearRect(0, 0, cWidth, cHeight + capHeight);
+      for (let i = 0; i < meterCount; i++) {
+        const value = array[i * step];
+        if (capYPositionArray.length < Math.round(meterCount)) {
+          capYPositionArray.push(value);
         }
-        const step = Math.round(array.length / meterCount);
-        ctx.clearRect(0, 0, cWidth, cHeight + capHeight);
-        for (let i = 0; i < meterCount; i++) {
-          const value = array[i * step];
-          if (capYPositionArray.length < Math.round(meterCount)) {
-            capYPositionArray.push(value);
-          }
-          ctx.fillStyle = capColor;
+        ctx.fillStyle = capColor;
 
-          if (value < capYPositionArray[i]) {
-            const preValue = --capYPositionArray[i];
-            const y = ((270 - preValue) * cHeight) / 270;
-            ctx.fillRect(i * (meterWidth + gap), y, meterWidth, capHeight);
-          } else {
-            const y = ((270 - value) * cHeight) / 270;
-            ctx.fillRect(i * (meterWidth + gap), y, meterWidth, capHeight);
-            capYPositionArray[i] = value;
-          }
-          ctx.fillStyle = gradient;
-          const y = ((270 - value) * cHeight) / 270 + capHeight;
-          ctx.fillRect(i * (meterWidth + gap), y, meterWidth, cHeight);
+        if (value < capYPositionArray[i]) {
+          const preValue = --capYPositionArray[i];
+          const y = ((270 - preValue) * cHeight) / 270;
+          ctx.fillRect(i * (meterWidth + gap), y, meterWidth, capHeight);
+        } else {
+          const y = ((270 - value) * cHeight) / 270;
+          ctx.fillRect(i * (meterWidth + gap), y, meterWidth, capHeight);
+          capYPositionArray[i] = value;
         }
-        animationId.current = requestAnimationFrame(drawMeter);
-      };
+        ctx.fillStyle = gradient;
+        const y = ((270 - value) * cHeight) / 270 + capHeight;
+        ctx.fillRect(i * (meterWidth + gap), y, meterWidth, cHeight);
+      }
       animationId.current = requestAnimationFrame(drawMeter);
-    },
-    [capColor, capHeight, gap, meterColor, meterCount, meterWidth]
-  );
+    };
+    animationId.current = requestAnimationFrame(drawMeter);
+  };
 
-  const setupAudioNode = React.useCallback(
-    (currentAudioEle: HTMLAudioElement) => {
-      if (!currentAudioEle) {
-        throw new Error("Audio element is not found");
-      }
-      if (!analyser.current && audioContext.current) {
-        analyser.current = audioContext.current.createAnalyser();
-        analyser.current.smoothingTimeConstant = 0.8;
-        analyser.current.fftSize = 2048;
-      }
+  const setupAudioNode = (currentAudioEle: HTMLAudioElement) => {
+    if (!currentAudioEle) {
+      throw new Error("Audio element is not found");
+    }
+    if (!analyser.current && audioContext.current) {
+      analyser.current = audioContext.current.createAnalyser();
 
-      if (!mediaEleSource.current && audioContext.current && analyser.current) {
-        mediaEleSource.current =
-          audioContext.current.createMediaElementSource(currentAudioEle);
-        mediaEleSource.current.connect(analyser.current);
-        mediaEleSource.current.connect(audioContext.current.destination);
-      }
+      /* 
+        Smoothing time constant is, essentially, an average 
+        between the current buffer and the last buffer the AnalyserNode processed, 
+        and results in a much smoother set of value changes over time.
+        
+        Minimum = 0
+        Maximum = 1
+        (For panic inducing bounce, try 0.1)
+      */
+      analyser.current.smoothingTimeConstant = smoothingTimeConstant;
+      /* 
+        1,024 samples (1k) is a pretty common frame size for an audio FFT. 
+        At a sample rate of 44.1 kHz, 1,024 samples is about 0.022 second of sound.
 
-      return analyser;
-    },
-    []
-  );
+        Minimum = 32
+        Maximum = 32768
+
+        1024 and 2048 Seem to be a good sweet spot for 
+        general smoothness but active spectrum bounce.
+      */
+      analyser.current.fftSize = fftSize;
+    }
+
+    if (!mediaEleSource.current && audioContext.current && analyser.current) {
+      mediaEleSource.current =
+        audioContext.current.createMediaElementSource(currentAudioEle);
+      mediaEleSource.current.connect(analyser.current);
+      mediaEleSource.current.connect(audioContext.current.destination);
+    }
+
+    return analyser;
+  };
 
   const prepareAPIs = React.useCallback(() => {
     try {
-      audioContext.current = new window.AudioContext(); // 1.set audioContext
+      audioContext.current = new window.AudioContext();
     } catch (e) {
-      console.error("!Your browser does not support AudioContext");
+      console.error("Your browser does not support AudioContext");
       console.error(e);
     }
   }, []);
-  const initAudioEvents = React.useCallback(() => {
+
+  // Retain old value to compare
+  const [meterColorState, setMeterColorState] = React.useState(meterColor);
+
+  const initAudioEvents = (trigger?: "color") => {
+    // Hyper-hacky way to reinit
+    if (trigger === "color" && audioEle.current) {
+      audioEle.current.pause();
+      audioEle.current.play();
+    }
     if (audioEle.current) {
       audioEle.current.onpause = () => {
         playStatus.current = "PAUSED";
@@ -183,12 +212,19 @@ export default function AudioSpectrum({
         drawSpectrum(currentAnalyser.current!);
       };
     }
-  }, [drawSpectrum, prepareAPIs, setupAudioNode]);
+  };
 
   React.useEffect(() => {
-    prepareElements();
-    initAudioEvents();
-  }, [prepareElements, initAudioEvents]);
+    // State keeps track of old color, won't update from original color unless user changes.
+    if (meterColor !== meterColorState) {
+      setMeterColorState(meterColor);
+      prepareElements();
+      initAudioEvents("color");
+    } else {
+      prepareElements();
+      initAudioEvents();
+    }
+  }, [prepareElements, initAudioEvents, meterColor, meterColorState]);
 
   return (
     <canvas
